747cec050bf2207663e99791f416acbc28c62a09
Harshit Bhardwaj <harshitbhardwaj97@gmail.com>
2024-01-01 00:03:49 +0530

    exercise 006 solution added

--- error-messages-tutorial/src/006-expression-of-type-x-cant-be-used-to-index-type-y.problem.ts
+++ error-messages-tutorial/src/006-expression-of-type-x-cant-be-used-to-index-type-y.problem.ts
@@ -1,4 +1,11 @@
-const productPrices = {
+type Product = Record<string, number>;
+
+/* interface Product {
+   [productName : string] : number;
+  }
+*/
+
+const productPrices: Product = {
   Apple: 1.2,
   Banana: 0.5,
   Orange: 0.8,
