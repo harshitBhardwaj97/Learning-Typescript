3944db944a8bbed0bbfee240b3c0ff15d9df0099
Harshit Bhardwaj <harshitbhardwaj97@gmail.com>
2024-01-01 12:30:35 +0530

    exercise 04 solution added

--- zod-tutorial/src/04-infer.problem.ts
+++ zod-tutorial/src/04-infer.problem.ts
@@ -4,13 +4,35 @@ import { z } from "zod";
 
 const StarWarsPerson = z.object({
   name: z.string(),
+  height: z.string(),
+  mass: z.string(),
+  hair_color: z.string(),
+  skin_color: z.string(),
+  eye_color: z.string(),
+  birth_year: z.string(),
+  gender: z.string(),
+  homeworld: z.string(),
+  films: z.array(z.string()),
+  species: z.array(z.string()),
+  vehicles: z.array(z.string()),
+  starships: z.array(z.string()),
+  created: z.string(),
+  edited: z.string(),
+  url: z.string(),
 });
 
+type Result = z.infer<typeof StarWarsPerson>;
+
 const StarWarsPeopleResults = z.object({
+  count: z.number(),
+  next: z.union([z.string(), z.null()]),
+  previous: z.union([z.string(), z.null()]),
   results: z.array(StarWarsPerson),
 });
 
-const logStarWarsPeopleResults = (data: unknown) => {
+type SWPeopleResult = z.infer<typeof StarWarsPeopleResults>;
+
+const logStarWarsPeopleResults = (data: SWPeopleResult) => {
   //                                    ^ 🕵️‍♂️
   data.results.map((person) => {
     console.log(person.name);
