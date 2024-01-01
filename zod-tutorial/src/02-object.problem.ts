62f58ecff811277b2e3646f76d2cfb3d75bd8bad
Harshit Bhardwaj <harshitbhardwaj97@gmail.com>
2024-01-01 11:30:19 +0530

    exercise 02 solution added

--- zod-tutorial/src/02-object.problem.ts
+++ zod-tutorial/src/02-object.problem.ts
@@ -3,15 +3,33 @@
 import { expect, it } from "vitest";
 import { z } from "zod";
 
-const PersonResult = z.unknown();
+const PersonResult = z.object({
+  name: z.string(),
+  height: z.string(),
+  mass: z.string(),
+  hair_color: z.string(),
+  skin_color: z.string(),
+  eye_color: z.string(),
+  birth_year: z.string(),
+  gender: z.string(),
+  homeworld: z.string(),
+  films: z.array(z.string()),
+  species: z.array(z.string()),
+  vehicles: z.array(z.string()),
+  starships: z.array(z.string()),
+  created: z.string(),
+  edited: z.string(),
+  url: z.string(),
+});
 //                   ^ 🕵️‍♂️
 
 export const fetchStarWarsPersonName = async (id: string) => {
   const data = await fetch(
-    "https://www.totaltypescript.com/swapi/people/" + id + ".json",
+    "https://www.totaltypescript.com/swapi/people/" + id + ".json"
   ).then((res) => res.json());
 
   const parsedData = PersonResult.parse(data);
+  console.log(parsedData);
 
   return parsedData.name;
 };
