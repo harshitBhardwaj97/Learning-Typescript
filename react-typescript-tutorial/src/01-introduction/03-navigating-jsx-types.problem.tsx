850bb7a2548b7761efc6acad389bcaef6c35cf17
Harshit Bhardwaj <harshitbhardwaj97@gmail.com>
2024-01-01 16:23:50 +0530

    exercise 03 solution added

--- react-typescript-tutorial/src/01-introduction/03-navigating-jsx-types.problem.tsx
+++ react-typescript-tutorial/src/01-introduction/03-navigating-jsx-types.problem.tsx
@@ -1,12 +1,14 @@
 export const Component = () => {
+  function handleChange() {}
+
   return (
     <div
       // How do I figure out what type aria-posinset expects?
-      aria-posinset={}
+      aria-posinset={1} // => By hovering over aria-posinet we find that it expects number | undefined
       // How do I figure out what type onChange expects?
-      onChange={}
+      onChange={handleChange} // => By hovering over onChange we find that it expects React.FormEventHandler<HTMLDivElement> | undefined
 
-      // How do I get autocomplete with JSX?
+      // How do I get autocomplete with JSX? => Ctrl+Space
     />
   );
 };
