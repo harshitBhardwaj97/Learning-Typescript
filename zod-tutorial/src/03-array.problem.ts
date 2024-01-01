c90af55268899670be0769400879bb10fab02445
Harshit Bhardwaj <harshitbhardwaj97@gmail.com>
2024-01-01 12:25:28 +0530

    exercise 03 solution added

--- zod-tutorial/src/03-array.problem.ts
+++ zod-tutorial/src/03-array.problem.ts
@@ -5,17 +5,40 @@ import { z } from "zod";
 
 const StarWarsPerson = z.object({
   name: z.string(),
+  height: z.string(),
+  mass: z.string(),
+  hair_color: z.string(),
+  skin_color: z.string(),
+  eye_color: z.string(),
+  birth_year: z.string(),
+  gender: z.string(),
+  homeworld: z.string(),
+  films: z.array(z.string()),
+  species: z.array(z.string()),
+  vehicles: z.array(z.string()),
+  starships: z.array(z.string()),
+  created: z.string(),
+  edited: z.string(),
+  url: z.string(),
 });
 
-const StarWarsPeopleResults = z.unknown();
+type Result = z.infer<typeof StarWarsPerson>;
+
+const StarWarsPeopleResults = z.object({
+  count: z.number(),
+  next: z.union([z.string(), z.null()]),
+  previous: z.union([z.string(), z.null()]),
+  results: z.array(StarWarsPerson),
+});
 //                            ^ 🕵️‍♂️
 
 export const fetchStarWarsPeople = async () => {
   const data = await fetch(
-    "https://www.totaltypescript.com/swapi/people.json",
+    "https://www.totaltypescript.com/swapi/people.json"
   ).then((res) => res.json());
 
   const parsedData = StarWarsPeopleResults.parse(data);
+  console.log(parsedData);
 
   return parsedData.results;
 };
@@ -25,5 +48,31 @@ export const fetchStarWarsPeople = async () => {
 it("Should return the name", async () => {
   expect((await fetchStarWarsPeople())[0]).toEqual({
     name: "Luke Skywalker",
+    height: "172",
+    mass: "77",
+    hair_color: "blond",
+    skin_color: "fair",
+    eye_color: "blue",
+    birth_year: "19BBY",
+    gender: "male",
+    homeworld: "https://swapi.dev/api/planets/1/",
+    films: [
+      "https://swapi.dev/api/films/1/",
+      "https://swapi.dev/api/films/2/",
+      "https://swapi.dev/api/films/3/",
+      "https://swapi.dev/api/films/6/",
+    ],
+    species: [],
+    vehicles: [
+      "https://swapi.dev/api/vehicles/14/",
+      "https://swapi.dev/api/vehicles/30/",
+    ],
+    starships: [
+      "https://swapi.dev/api/starships/12/",
+      "https://swapi.dev/api/starships/22/",
+    ],
+    created: "2014-12-09T13:50:51.644000Z",
+    edited: "2014-12-20T21:17:56.891000Z",
+    url: "https://swapi.dev/api/people/1/",
   });
 });
